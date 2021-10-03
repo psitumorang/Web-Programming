8ad596e70281208e5ad2dddd4a23bfd102f90995
Morgan McLees <mcleesm@seas.upenn.edu>
2021-10-03 17:13:46 -0400

    decorative art created

--- login-page/src/LoginPage.js
+++ login-page/src/LoginPage.js
@@ -5,7 +5,14 @@ import LoginForm from './LoginForm.js';
 function LoginPage() {
   return (
     <div className="LoginPage">
-      <div id="artSide"></div>
+      <div id="artSide">
+        <div className="rectWrapper"> 
+            <div className="rect" id="rect1"></div>
+            <div className="rect" id="rect2"></div>
+            <div className="rect" id="rect3"></div>
+            <div className="rect" id="rect4"></div>
+        </div>
+      </div>
       <div id="loginFormContainer">
         <LoginForm id="loginForm"/>
       </div>
