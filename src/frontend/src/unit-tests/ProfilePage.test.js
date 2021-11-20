7bdcd0015fdf5a331869ec99bb5f352309fa1891
MaxRigby <33171494+MaxRigby@users.noreply.github.com>
2021-11-20 14:51:45 -0500

    Update ProfilePage.test.js

--- src/frontend/src/unit-tests/ProfilePage.test.js
+++ src/frontend/src/unit-tests/ProfilePage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ProfilePage from './ProfilePage';
+import ProfilePage from '../ProfilePage';
 
 /**
  * @jest-environment jsdom
